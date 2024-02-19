11c4dda1b73f530ed62be86b39aa0e1ee8dfba7d
SolomonChidera <solochidera10@gmail.com>
2024-02-19 01:44:55 +0100

    Foundamental code Handled bot not responding to it self Getting it online

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Discord = require('discord.js');
-const { token } = require('./config.json'); // You'll store your token here
+require('dotenv').config();
+const token = process.env.TOKEN; // You'll store your token here
 
 const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMessages] });
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Ready! Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', async message => {
-    if (message.author.bot) return; // Prevent the bot from responding to itself
+  if (message.author.bot) return; // Prevent the bot from responding to itself
 
-    // Add your command handling and moderation logic here 
+  // Add your command handling and moderation logic here
 });
 
 client.login(token);
